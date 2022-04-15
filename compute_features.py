29860b81dc43b505f7b38e934293f6989d8d36fd
Benoit Malezieux <bmalezie@drago5.saclay.inria.fr>
2022-04-15 17:10:29 +0200

    Feature engineering

---
+++ compute_features.py
@@ -0,0 +1,48 @@
+import mne_bids
+import mne
+
+import numpy as np
+import pandas as pd
+
+import pathlib
+
+DERIV_ROOT = pathlib.Path('/storage/store3/derivatives/biomag_hokuto_bids')
+BIDS_ROOT = pathlib.Path(
+    '/storage/store/data/biomag_challenge/Biomag2022/biomag_hokuto_bids'
+)
+
+
+def compute_dummy_features(epochs):
+    return np.random.random(10)
+
+
+def run_subject(subject, feature_type):
+    
+    bids_path = mne_bids.BIDSPath(
+        subject=subject,
+        root=DERIV_ROOT,
+        datatype="meg",
+        task="rest",
+        session="rest",
+        processing="clean",
+        suffix="epo",
+        extension=".fif"
+    )
+    epochs = mne.read_epochs(bids_path)
+    
+    if feature_type == "random":
+        out = compute_dummy_features(epochs)
+        
+    return out
+
+
+if __name__ == "__main__":
+    
+    participants = pd.read_csv(BIDS_ROOT / "participants.tsv", sep="\t")
+    features = []
+    feature_type = "random"
+    for subject in participants.participant_id:
+        features.append(run_subject(subject, feature_type)[None, :])
+    features = np.concatenate(features, axis=0)
+    np.save(DERIV_ROOT / f"features_{feature_type}.npy")
+    
