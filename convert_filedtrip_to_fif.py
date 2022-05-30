bbeb39d8a7f03f4e96bb3de004b1c39c77234e24
Apolline Mellot <apolline.mellot@inria.fr>
2022-05-30 13:57:17 +0200

    clean 1

--- convert_filedtrip_to_fif.py
+++ convert_filedtrip_to_fif.py
@@ -1,4 +1,3 @@
-import numpy as np
 import mne
 import os
 
@@ -6,36 +5,36 @@ from pathlib import Path
 
 base_path = Path("/storage/store/data/biomag_challenge/Biomag2022")
 
-converted_path = base_path / "biomag_hokuto_fieldtrip"
-target_path = base_path / "biomag_hokuto_fif"
+path_fieldtrip = base_path / "biomag_hokuto_fieldtrip"
+path_fif = base_path / "biomag_hokuto_fif"
 
-os.mkdir(target_path / "training")
-os.mkdir(target_path / "training" / "control")
-os.mkdir(target_path / "training" / "dementia")
-os.mkdir(target_path / "training" / "mci")
-os.mkdir(target_path / "test")
+os.mkdir(path_fif / "training")
+os.mkdir(path_fif / "training" / "control")
+os.mkdir(path_fif / "training" / "dementia")
+os.mkdir(path_fif / "training" / "mci")
+os.mkdir(path_fif / "test")
 
 
-def create_raw(raw_data):
-    raw_copy = raw_data.pick_types(meg=True)
+def create_raw(raw_fieldtrip):
+    raw_copy = raw_fieldtrip.pick_types(meg=True)
     raw_copy.load_data()
     
     data = raw_copy.get_data()
-    data *= 1e-15
+    data *= 1e-15 # fT to T 
 
     ch_types = raw_copy.get_channel_types()
     ch_names = raw_copy.ch_names
     info = mne.create_info(ch_names=ch_names, ch_types=ch_types, sfreq=1000)
 
-    raw_meg = mne.io.RawArray(data, info)
-    return raw_meg
+    raw_fif = mne.io.RawArray(data, info)
+    return raw_fif
 
 
-for subdir, dirs, files in os.walk(str(converted_path)):
+for subdir, dirs, files in os.walk(str(path_fieldtrip)):
     for file in files:
         if file.endswith(".mat"):
-            raw_data = mne.io.read_raw_fieldtrip(Path(subdir) / file, None)
-            raw_meg = create_raw(raw_data)
+            raw_fieldtrip = mne.io.read_raw_fieldtrip(Path(subdir) / file, None)
+            raw_fif = create_raw(raw_fieldtrip)
             new_path = subdir.replace("biomag_hokuto_fieldtrip", "biomag_hokuto_fif")
             new_path = new_path + '/' + file
-            raw_meg.save(new_path.replace(".mat", ".fif"))
+            raw_fif.save(new_path.replace(".mat", ".fif"))
